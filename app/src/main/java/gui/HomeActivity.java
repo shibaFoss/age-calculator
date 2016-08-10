7624736e7b1539789b6a4a5e6d8bea771f57cff3
Shiba Prasad Jana <shiba.developer@hotmail.com>
2016-08-11 04:37:26 +0530

    Building the projct for the first time

---
+++ app/src/main/java/gui/HomeActivity.java
@@ -0,0 +1,16 @@
+package gui;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * The class is the UI class where user will interact with the age calculation process.
+ */
+public class HomeActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle bundle) {
+        super.onCreate(bundle);
+
+    }
+}
