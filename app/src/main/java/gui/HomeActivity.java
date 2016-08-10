5343759ecbc1f4bb8a2ba190449215e55a2617f4
Shiba Prasad Jana <shiba.developer@hotmail.com>
2016-08-11 04:56:37 +0530

    added the license.

--- app/src/main/java/gui/HomeActivity.java
+++ app/src/main/java/gui/HomeActivity.java
@@ -8,9 +8,23 @@ import android.os.Bundle;
  */
 public class HomeActivity extends Activity {
 
+    /**
+     * The method get called by the system when user enter the app for the first time.
+     * @param bundle the system send a {@link Bundle} object associate
+     *               with this {@link HomeActivity} class.
+     */
     @Override
     public void onCreate(Bundle bundle) {
         super.onCreate(bundle);
 
     }
+
+    /**
+     * The method get called by the system when user press the back button to exit the app.
+     */
+    @Override
+    public void onBackPressed() {
+
+    }
+
 }
